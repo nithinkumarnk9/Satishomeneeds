c070b3a27bd6669337546c9fbfe34e2d57f63208
Nithin kumar <nithinkumarnk999@gmail.com>
2024-08-24 23:11:04 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/")
+def about():
+    return render_template("about.html")
+@app.route("/")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
